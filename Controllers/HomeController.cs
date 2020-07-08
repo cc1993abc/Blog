bd0d4fd1339bbcf8a3ca95b5fdb18a3cf4d496e7
超 陈 <a18674130101@outlook.com>
2020-07-09 07:46:56 +0800

    添加项目文件。

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Blog.Controllers.Repository;
+using Blog.Data;
+using Blog.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IRepository _repo { get; set; }
+        public HomeController(IRepository repo)
+        {
+            _repo = repo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Post()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            return View(new Post());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Post post)
+        {
+            _repo.AddPost(post);
+
+            if(await _repo.SaveChangesAsync())
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View("Post");
+        }
+    }
+}
---
+++ Controllers/Repository/Repository.cs
@@ -0,0 +1,53 @@
+﻿using Blog.Data;
+using Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Controllers.Repository
+{
+    public class Repository : IRepository
+    {
+        private AppDbContext _ctx { get; set; }
+        public Repository(AppDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public void AddPost(Post post)
+        {
+           _ctx.posts.Add(post);
+        }
+
+        public List<Post> GetAllPosts(int id)
+        {
+            return _ctx.posts.ToList();
+        }
+
+        public Post GetPost(int id)
+        {
+            return _ctx.posts.FirstOrDefault(p => p.Id == id);
+
+        }
+
+        public void RemovePost(int id)
+        {
+            _ctx.posts.Remove(GetPost(id));
+        }
+
+        public void UpdatePost(Post post)
+        {
+            _ctx.posts.Update(post);
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            if(await _ctx.SaveChangesAsync() > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
